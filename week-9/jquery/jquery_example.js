ccd716708a28f2483066c0749ef39c4ffbaa25c8
Dan Turcza <datu925@gmail.com>
2016-01-16 18:45:27 -0600

    Update to jquery challenge

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -18,19 +18,38 @@ $('body').css({'background-color': 'pink'})
 //RELEASE 2:
   //Add code here to select elements of the DOM
   bodyElement = $('body')
-  //mytest = $('img\\.src')
+  image = $('img')
+  image.css("position", "absolute")
+  image.css("left", "0")
 
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
-
+  mytitle = $('body > h1')
+  mytitle.css("background-color", "white")
+  mytitle.css("border", "5px solid black")
+  mytitle.css("visibility", "visible")
+  $('div.mascot h1').html("Squirrels!")
 
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
 
 
+  // $('img').hover(function(e){
+  //   e.preventDefault()
+  //   $(this).attr('src', 'http://www.wormsandgermsblog.com/files/2010/09/Baby-squirrel1.jpg')
+  // }, function(e){
+  //   e.preventDefault()
+  //   $(this).attr('src', 'dbc_logo.png')
+  // })
+
 //RELEASE 5: Experiment on your own
 
-
+$('img').click(function() {
+  $('img').animate({
+    left: "500px",
+    height: "0px"
+  }, 1500, "linear");
+})
 
 
 
